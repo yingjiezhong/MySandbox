997d13e4a48fefe58865b23cbb724bea69807af8
Yingjie Zhong <xining.zhong@gmail.com>
2020-10-08 21:55:12 -0400

    deadlock3 works fine - lock as class

---
+++ gradle-java-console-application/gradlejava1-initial/src/main/java/com/denofprogramming/thread/DeadLock3.java
@@ -0,0 +1,45 @@
+package com.denofprogramming.thread;
+
+public class DeadLock3 {
+
+        void method1() {
+            synchronized(String.class) {
+                System.out.println(Thread.currentThread().getName() + " acquired String lock");
+                synchronized(Integer.class) {
+                    System.out.println(Thread.currentThread().getName() + " acquired Integer lock");
+                }
+            }
+        }
+
+    void method2() {
+        synchronized(Integer.class) {
+            System.out.println(Thread.currentThread().getName() + " acquired Integer lock");
+            synchronized(String.class) {
+                System.out.println(Thread.currentThread().getName() + " acquired String lock");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DeadLock3 d3 = new DeadLock3();
+
+        Runnable r1 = () -> d3.method1();
+
+        Runnable r2 = () -> d3.method2();
+
+        Thread t1 = new Thread(r1);
+        t1.setName("T1");
+        Thread t2 = new Thread(r2);
+        t2.setName("T2");
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
