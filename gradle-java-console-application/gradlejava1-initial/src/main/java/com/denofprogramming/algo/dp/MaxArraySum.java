ecb8eb5941d8f14420c48e610b4d9b8a8858064e
Yingjie Zhong <xining.zhong@gmail.com>
2020-10-24 17:49:02 -0400

    added good solution for MaxArraySum

--- gradle-java-console-application/gradlejava1-initial/src/main/java/com/denofprogramming/algo/dp/MaxArraySum.java
+++ gradle-java-console-application/gradlejava1-initial/src/main/java/com/denofprogramming/algo/dp/MaxArraySum.java
@@ -19,25 +19,45 @@ public class MaxArraySum {
      * @param a
      * @return
      */
-    public static int sum(int[] a) {
+    public static int sum(int[] arr) {
 
-        int n = a.length;
+        int n = arr.length;
         int max = 0;
         int[][] t = new int[n][n];
 
         // fill in the top half of the matrix
-        for (int i = 0; i < n; i++) {
-            for (int j=i; j < n; j++) {
-                if (i == j || j == i+1) {
-                    t[i][j] = 0;
-                } else if (j == i+2 || j == i+3){
-                    t[i][j] = a[i] + a[j];
-                } else {
-                    t[i][j] = a[j] + Math.max(Math.max(t[i][j-3], t[i][j-2]), a[i]);
+        if (arr.length == 0) {
+            return 0;
+        } else if (arr.length == 1) {
+            return arr[0];
+        }else if (arr.length == 2) {
+            return Math.max(arr[0], arr[1]);
+        } else {
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i; j < n; j++) {
+                    if (i == j || j == i + 1) {
+                        t[i][j] = 0;
+                    } else if (j == i + 2 || j == i + 3) {
+                        t[i][j] = arr[i] + Math.max(0, arr[j]);
+                    } else {
+                        t[i][j] = Math.max(0, arr[j]) + Math.max(Math.max(t[i][j - 3], t[i][j - 2]), arr[i]);
+                    }
+                    max = Math.max(max, t[i][j]);
                 }
-                max = Math.max(max, t[i][j]);
             }
         }
+
         return max;
+    }
+
+    public static int maxSubsetSum(int[] arr) {
+        if (arr.length == 0) return 0;
+        arr[0] = Math.max(0, arr[0]);
+        if (arr.length == 1) return arr[0];
+        arr[1] = Math.max(arr[0], arr[1]);
+        for (int i = 2; i < arr.length; i++)
+            arr[i] = Math.max(arr[i-1], arr[i]+arr[i-2]);
+        return arr[arr.length-1];
     }
 }
